ca311496a87f13070c61d1bb3d2a88142206f138
Tecnomi <67217738+pranay-tecnomi@users.noreply.github.com>
2026-09-01 17:24:09 +0530

    feat(m0): bootstrap Rust recovery core and storage I/O

---
+++ crates/recovery-core/src/ids.rs
@@ -0,0 +1,29 @@
+use core::fmt;
+
+macro_rules! opaque_id {
+    ($name:ident) => {
+        #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+        pub struct $name(String);
+
+        impl $name {
+            pub fn new(value: impl Into<String>) -> Self {
+                Self(value.into())
+            }
+
+            pub fn as_str(&self) -> &str {
+                &self.0
+            }
+        }
+
+        impl fmt::Display for $name {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                f.write_str(&self.0)
+            }
+        }
+    };
+}
+
+opaque_id!(SourceId);
+opaque_id!(ScanSessionId);
+opaque_id!(CandidateId);
+opaque_id!(RecoveryJobId);
