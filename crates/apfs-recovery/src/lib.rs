175838212b42759a4636d888f2676573d950178e
Tecnomi <67217738+pranay-tecnomi@users.noreply.github.com>
2026-09-05 23:13:22 +0530

    Export APFS filesystem index

--- crates/apfs-recovery/src/lib.rs
+++ crates/apfs-recovery/src/lib.rs
@@ -5,12 +5,14 @@ use storage_io::BlockDevice;
 
 mod btree;
 mod catalog;
+mod filesystem;
 mod fsrecord;
 mod omap;
 mod omap_lookup;
 mod volume_omap;
 pub use btree::{btree_entries, btree_fixed_entries, btree_variable_entries, parse_btree_node, ApfsBtreeEntry, ApfsBtreeNode, ApfsBtreeVariableEntry, ApfsFixedBtreeEntry, ApfsVariableBtreeEntry};
 pub use catalog::{read_catalog_records, ApfsCatalogRecord};
+pub use filesystem::{decode_file_extent_key, index_catalog_records, ApfsDirectoryEntry, ApfsFileExtent, ApfsFilesystemIndex};
 pub use fsrecord::{decode_drec_key, decode_hashed_drec_key, decode_dir_record_value, decode_file_extent_value, decode_inode_value, decode_jkey, extent_is_sparse, extent_length, ApfsDrecKey, ApfsDirRecordValue, ApfsFileExtentValue, ApfsInodeValue, ApfsJKey, APFS_TYPE_DIR_REC, APFS_TYPE_FILE_EXTENT, APFS_TYPE_INODE};
 pub use omap::{parse_object_map, parse_object_map_key, parse_object_map_value, ApfsObjectMap, ApfsObjectMapKey, ApfsObjectMapValue};
 pub use omap_lookup::lookup_object_map;
